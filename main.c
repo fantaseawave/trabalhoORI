ee0309fa2f8ed963778750a0fc51c1ae069746a6
João Henrique <110070981+fantaseawave@users.noreply.github.com>
2023-08-22 19:21:16 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,79 +1,74 @@
 #include <stdio.h>
 #include "ArvB.h"
 
-char posicao_alfabeto(char letra){
-    return (letra-'a'+1);
-}
-
 int main() {
 
-   
-  char vetor1[] = {'o', 'k', 'i'};
-  char vetor2[] = {'g', 'r', 'y'};   
-  char vetor3[] = {'p', 'x', 'w', 'e', 'l', 'd', 'b'};
-  char vetor4[] = {'j', 't'};
-  char vetor5[] = {'m', 'v', 'z', 'h', 's', 'c'};
-  char vetor6[] = {'q', 'u', 'n', 'f', 'a'};
+  ArvB* arvore = arvB_cria();
 
-  ArvB *arvore1 = arvB_cria();
-  (*arvore1)->qtd_chaves = 1; //Não está sendo atribuido devido ao erro "member reference base type 'ArvB' (aka 'struct NO *') is not a structure or union"
-  struct NO no1; 
-  no1.qtd_chaves = 3;
-  no1.chaves[0] = vetor3[0];
-  no1.chaves[1] = vetor3[1];
-  no1.chaves[2] = vetor3[2];
-
-  //*arvore1 = &no1;
-
-
-  /*for (int i = 0; i <= sizeof(vetor6); i++){
-    arvB_insere(arvore1, posicao_alfabeto(vetor6[i]));
-  }*/
-
-
-  arvB_insere(arvore1, posicao_alfabeto(vetor3[0]));
-  arvB_insere(arvore1, posicao_alfabeto(vetor3[1]));
-  arvB_insere(arvore1, posicao_alfabeto(vetor3[2]));
-  arvB_insere(arvore1, posicao_alfabeto(vetor3[3]));
-  arvB_insere(arvore1, posicao_alfabeto(vetor3[4]));
-  arvB_insere(arvore1, posicao_alfabeto(vetor3[5]));
-  arvB_insere(arvore1, posicao_alfabeto(vetor3[6]));
-
-    
-    printf("Árvore B 1: \n");
-    arvB_imprime(arvore1);
   
+    arvB_insere(arvore, 13);
+    arvB_insere(arvore, 26);
+    arvB_insere(arvore, 74);
+    arvB_insere(arvore, 19);
+    arvB_insere(arvore, 12);
+    arvB_insere(arvore, 75);
+    arvB_insere(arvore, 86);
+    arvB_insere(arvore, 31);
+    arvB_insere(arvore, 64);
+    arvB_insere(arvore, 47);
+    arvB_insere(arvore, 77);
+    arvB_insere(arvore, 94);
+    arvB_insere(arvore, 53);
+    arvB_insere(arvore, 69);
+    arvB_insere(arvore, 20);
+    arvB_insere(arvore, 30);
+    arvB_insere(arvore, 10);
+    arvB_insere(arvore, 92);
+    arvB_insere(arvore, 22);
+    arvB_insere(arvore, 29);
+    arvB_insere(arvore, 71);
+    arvB_insere(arvore, 15);
+    arvB_insere(arvore, 35);
+    arvB_insere(arvore, 60);
+    arvB_insere(arvore, 64);
+    arvB_insere(arvore, 67);
   
-  printf("\nQuantidade de nós: %d\n", arvB_qtd_nos(*arvore1));
-  printf("\nQuantidade de chaves: %d\n", arvB_qtd_chaves(*arvore1));
+
+  printf("Árvore B: \n");
+  arvB_imprime(arvore);
+
   
-  int valorBuscado = posicao_alfabeto('l');
+  printf("\nQuantidade de nós: %d\n", arvB_qtd_nos(*arvore));
+  printf("\nQuantidade de chaves: %d\n", arvB_qtd_chaves(*arvore));
   
-  if(arvB_busca(*arvore1, valorBuscado)){
+  int valorBuscado = 29;
+
+  
+  if(arvB_busca(*arvore, valorBuscado)){
     printf("\nO valor %d se encontra na árvore!\n", valorBuscado);
   } else {
     printf("\nO valor %d não se encontra na árvore!\n", valorBuscado);
   }
 
-
-  if(arvB_remove(arvore1, valorBuscado)){
+  if(arvB_remove(arvore, valorBuscado)){
     printf("\nO valor %d foi removido da árvore!\n", valorBuscado);
     } else {
     printf("\nO valor %d não foi removido pois não foi encontrado na árvore!\n", valorBuscado);
   }
 
-   if(arvB_busca(*arvore1, valorBuscado)){
+   if(arvB_busca(*arvore, valorBuscado)){
     printf("\nO valor %d se encontra na árvore!\n", valorBuscado);
   } else {
     printf("\nO valor %d não se encontra na árvore!\n", valorBuscado);
   }
 
-  printf("\nQuantidade de nós: %d\n", arvB_qtd_nos(*arvore1));
-  printf("\nQuantidade de chaves: %d\n", arvB_qtd_chaves(*arvore1));
+  printf("\nQuantidade de nós: %d\n", arvB_qtd_nos(*arvore));
+  printf("\nQuantidade de chaves: %d\n", arvB_qtd_chaves(*arvore));
 
-  printf("\nÁrvore B final 1: \n");
-  arvB_imprime(arvore1);
+  printf("\nÁrvore B final: \n");
+  arvB_imprime(arvore);
   
-  arvB_destroi(arvore1);
+  arvB_destroi(arvore);
 
+  
 }
